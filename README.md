4a185c053043bb88f5c52a8028b4bb85691d9d56
M-Vigneshwaran1996 <vigneshwaran.mariappan96@gmail.com>
2025-05-02 14:37:00 +0530

    Added the APIs and services

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# bank-pay
+# Bank-pay
+
+In this project I created a structured streaming the transactions.<br>
+
+- Producer:<br>
+Simple Flask App by transactions through API.<br>
+Flask App get that data and load to Kafka topic for every transaction.<br>
+
+- Consumer:<br>
+Kafka Consumer subscribe the kafka Producer and waiting for message data<br>
+Consumer fetch the data and writes to Elasticsearch.<br>
+
+- Visualization:<br>
+Kibana to visualize Elasticsearch data<br>
+
+Data Flow
+
+[Client] → [Flask API] → [Kafka Producer] → [Kafka Topic]
+                                      ↓
+                               [Kafka Consumer]
+                                      ↓
+                              [Store to Elasticsearch]
+                                      ↓
+                                   [Kibana Visualization]
+
+<b>$ sudo docker-compose up -d --build</b> 
+
+# Usage
+Flask App: python flask-app/app.py<br>
+Consumer : python kafka-consumer/consumer.py<br>
+Kibana Dashboard: localhost:5602 <br>
---
+++ kafka-consumer/consumer.py
@@ -0,0 +1,36 @@
+from elasticsearch import Elasticsearch
+from kafka import KafkaConsumer
+import json
+
+es = Elasticsearch(
+    "http://localhost:9200",
+    basic_auth=("elastic", "c+pKBtFE8U9TnQ2j29ok"), verify_certs=False)
+
+if not es.ping():
+    print("*********Connection failed")
+    raise ValueError("Connection failed")
+else:
+    print("Connected to Elasticsearch")
+
+
+
+if not es.indices.exists(index='payment_transactions'):
+    es.indices.create(index='payment_transactions', ignore=400)
+
+consumer = KafkaConsumer(
+    'payment_transactions',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='transactions_group_1',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+print("*********Kafka consumer started and subscribed to topics:", consumer.subscription())
+
+
+for message in consumer:
+    transaction = message.value
+    print("Received transaction:**********", transaction)
+    es.index(index='payment_transactions', body=transaction)
+    print(f"Indexed transaction: {transaction}")
